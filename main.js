26c81d0261b89c1b2acb86266d186a3165915cf5
nfoster1219 <63244300+nfoster1219@users.noreply.github.com>
2020-07-14 09:10:30 -0500

    first commit

--- main.js
+++ main.js
@@ -19,20 +19,17 @@ function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
-
-// YOUR CODE GOES HERE
-
-
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+replaceAllButton.addEventListener("click", function () {
+    let find = findInput.value
+    let replace = replaceInput.value
+    for (let i = 0; i < rowElements.length; i ++) {
+        let rowArray = getCellElements(rowElements[i])
+        for (let index = 0; index < rowArray.length; index ++) {
+            let cell = rowArray[index]
+            if (cell.innerText.includes(find)) {
+                let replaceCell = cell.innerHTML.replace(find, replace)
+                cell.innerHTML = replaceCell
+            }
+        }
+    }
+})
