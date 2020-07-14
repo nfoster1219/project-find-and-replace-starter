306b966b717eeaca7253dd49cff815332b21bbb6
nfoster1219 <63244300+nfoster1219@users.noreply.github.com>
2020-07-14 09:13:26 -0500

    final commit

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
-replaceAllButton.addEventListener("click", function () {
+replaceButton.addEventListener("click", function () {
     let find = findInput.value
     let replace = replaceInput.value
     for (let i = 0; i < rowElements.length; i ++) {
